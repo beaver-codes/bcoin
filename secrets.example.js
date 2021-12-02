337d37f269e5f165abfccaec02a690a897b73ff6
Pavel Schoffer <xschof00@gmail.com>
2021-12-02 19:44:29 +0100

    secrets

---
+++ secrets.example.js
@@ -0,0 +1,7 @@
+const PRIVATE_KEYS = ['<your private key here>'];
+const INFURA_ENDPOINT = 'https://rinkeby.infura.io/v3/<your token here>';
+
+module.exports = {
+    PRIVATE_KEYS,
+    INFURA_ENDPOINT
+}
