2c04cd8aa9f256e1873a1b77458b86bbd205e3ee
Pavel Schoffer <xschof00@gmail.com>
2021-12-03 10:35:28 +0100

    Fix id

--- truffle-config.js
+++ truffle-config.js
@@ -68,7 +68,7 @@ module.exports = {
     },
     bsc_test: {
       provider: () => new HDWalletProvider({ privateKeys: PRIVATE_KEYS, providerOrUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545' }),
-      network_id: 4,       // Ropsten's id
+      network_id: 97,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
